fc2e603bed0199226f92d638b420d7f6fd47eb4d
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-09-01 23:07:36 +0300

    initial push

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('', views.homepage, name = 'home'),
+    path('signup/', views.SignupView.as_view(), name='signup' ),
+    path('profile/', views.UpdateProfile.as_view(), name='profile'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
